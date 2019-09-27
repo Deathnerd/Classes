4997a55dad61ef4f1029858a9626d4c4da650dc7
Wes Gilleland <wgillela@opentext.com>
2019-09-27 12:25:47 -0400

    Update

--- Classes.ps1
+++ Classes.ps1
@@ -3,6 +3,24 @@ using namespace System.IO
 using namespace System.Text
 using namespace System
 Write-Verbose "Creating Classes"
+
+class JenkinsBrowserLog {
+    [ValidateNotNullOrEmpty()]
+    [regex]$ParsePattern = "^(?<datetime>(?<day_name>[SMTWF][a-z]{2})\s(?<month_name>[JFMASOND][a-z]{2}))\s(?<day>\d+)\s(?<time>(?<hour>\d+):(?<minute>\d+):(?<second>\d+)\s(?<timezone>[A-Z]{3})\s(?<year>\d{4}))\s(?<severity>[A-Z]+)\s(?<message>.*)$"
+    [ValidateNotNullOrEmpty()]
+    [uri]$Link
+    hidden [string]$_content
+    JenkinsBrowserLog([uri]$Link) {
+        $this.Link = $Link
+        $this | Add-Member -MemberType ScriptProperty -Name Content -Value {
+            if (!$this._content) {
+                $this._content = Invoke-WebRequest $Link | Select-Object Content
+            }
+            return $this._content
+        }
+    }
+}
+
 class PathTransformAttribute : ArgumentTransformationAttribute {
     [object] Transform([EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
         if ( $inputData -is [string] ) {
@@ -21,19 +39,13 @@ class PathTransformAttribute : ArgumentTransformationAttribute {
         throw [FileNotFoundException]::new()
     }
 }
-function Get-ResolvedPath {
-    [CmdletBinding()]
-    Param(
-        [ValidateNotNullOrEmpty()]
-        [string]$Path
-    )
-    return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
-}
 
 class FuturePathTransformAttribute : ArgumentTransformationAttribute {
     [object] Transform([EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
         if ( $inputData -is [string] -and -NOT [string]::IsNullOrWhiteSpace( $inputData )) {
-            return Get-ResolvedPath $inputData
+            return ([scriptblock] {
+                    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($inputData)
+                })
         }
         throw [ArgumentException]::new($inputData + " needs to be a non-empty string")
     }
@@ -55,51 +67,12 @@ class EncodingTransformAttribute : ArgumentTransformationAttribute {
     }
 }
 
-class ModuleEntry {
-    [String] $Name
-    [boolean] $MayNeedInstall
-    [scriptblock] $Configuration
-    ModuleEntry([String]$Name, [Boolean]$MayNeedInstall) {
-        $this.Name = $Name
-        $this.MayNeedInstall = $MayneedInstall
-        $this.Configuration = [scriptblock]::Create("")
-    }
-
-    ModuleEntry([String]$Name, [Boolean]$MayNeedInstall, [scriptblock]$Configuration) {
-        $this.Name = $Name
-        $this.MayNeedInstall = $MayneedInstall
-        $this.Configuration = $Configuration
-    }
-    [boolean] isInstalled() {
-        if($this.Name -in $global:InstalledModules) {
-            return $true
-        } elseif($null -ne (Get-Module -ListAvailable -Name $this.Name)) {
-            #This wasn't in cache so update it
-            $global:InstalledModules += $this.Name
-            $global:InstalledModules | ConvertTo-Json > "$PSScriptRoot\InstalledModules.json"
-            return $true
-        }
-        return $false
-    }
-}
-
-class NetworkInterfaceIPInfo {
-    [string]$Name
-    [string]$Description
-    [string[]]$IPAddresses
-    NetworkInterfaceIPInfo([string]$Name, [string]$Description, [string[]]$IPAddresses) {
-        $this.Name = $Name
-        $this.Description = $Description
-        $this.IPAddresses = $IPAddresses
-    }
-}
-
 class DriveMapPair {
-    [DirectoryInfo] $UncPath;
+    [IO.DirectoryInfo] $UncPath;
     [DriveInfo] $DrivePath;
     [string] $Name
 
-    DriveMapPair([DirectoryInfo]$UncPath, [DriveInfo]$DrivePath) {
+    DriveMapPair([IO.DirectoryInfo]$UncPath, [DriveInfo]$DrivePath) {
         $this.DrivePath = $DrivePath
         $this.UncPath = $UncPath
         $this.Name = $UncPath -split "\\" | Select-Object -Last 1
